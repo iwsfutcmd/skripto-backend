6350695e4db5de5a31e5b2ce577b0c554333c972
Ben Yang <beyang@adobe.com>
2023-01-17 12:55:50 -0800

    added a bunch more languages and scripts

--- index.py
+++ index.py
@@ -4,41 +4,60 @@ import random
 from aksharamukha import transliterate, GeneralMap
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import unicodedataplus as unicodedata
+
+
+def get_script_of_word(word):
+    return {unicodedata.script(cp) for cp in word} - {"Common", "Inherited"}
+
 
 locale_files = glob("data/wiktionary/*")
 locales = [Path(d).name for d in locale_files]
 wordlists = {}
 for locale_file in locale_files:
+    locale = Path(locale_file).name
+    wordlists[locale] = {}
     forms = []
     weights = []
     with open(locale_file) as datafile:
         for line in datafile.readlines():
-            if not line: continue
+            if not line:
+                continue
             try:
                 form, weight = line.strip().split("\t")
             except ValueError:
                 form = line.strip()
                 weight = 1
-            forms.append(form)
-            weights.append(int(weight))
-    wordlists[Path(locale_file).name] = (forms, weights)
+            scripts = get_script_of_word(form)
+            if len(scripts) == 1:
+                script = scripts.pop()
+                unnormalized_script = script
+                script = script.replace("_", "")
+                if script not in GeneralMap.IndicScripts:
+                    script = unicodedata.property_value_aliases["script"][
+                        unnormalized_script
+                    ][0]
+            else:
+                script = "Zyyy"
+            if script == "Syrc":
+                script = "Syre"
+            if script not in wordlists[locale]:
+                wordlists[locale][script] = ([], [])
+            wordlists[locale][script][0].append(form)
+            wordlists[locale][script][1].append(int(weight))
 
-script_map = {
-    "ta": "Tamil",
-    "pa": "Gurmukhi",
-    "te": "Telugu",
-    "ml": "Malayalam",
-    "mr": "Devanagari",
-    "sa": "Devanagari",
-    "si": "Sinhala",
-    "th": "Thai",
-    "kn": "Kannada",
-    "or": "Oriya",
-    "gu": "Gujarati",
-    "bn": "Bengali",
-}
+script_map = {}
+for locale in wordlists:
+    script_map[locale] = []
+    total_entries = 0
+    for script in wordlists[locale]:
+        total_entries += len(wordlists[locale][script][0])
+    for script in wordlists[locale]:
+        if len(wordlists[locale][script][0]) / total_entries > 0.1:
+            script_map[locale].append(script)
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/", methods=["POST", "GET"])
 def serve_transl():
@@ -46,26 +65,53 @@ def serve_transl():
     to_script = request.json["to"]
     text = request.json["text"]
     if text:
-        return jsonify(transliterate.process(from_script, script_map[to_script], text))
+        return jsonify(transliterate.process(from_script, to_script, text))
     else:
         return jsonify("")
 
-@app.route("/scripts/to")
-def serve_to_scripts():
-    return jsonify(GeneralMap.LatinScripts + GeneralMap.MainIndic + list(GeneralMap.semiticISO.keys()))
+
+@app.route("/scripts")
+def serve_scripts():
+    return jsonify(
+        list(
+            set(
+                GeneralMap.LatinScripts
+                + GeneralMap.MainIndic
+                + GeneralMap.EastIndic
+                + GeneralMap.NonIndic
+                + GeneralMap.SemiticScripts
+                + GeneralMap.Roman
+                + list(GeneralMap.semiticISO.keys())
+            )
+        )
+    )
+
 
 @app.route("/locales")
 def serve_locales():
-    return jsonify(locales)
+    return jsonify(list(script_map.items()))
+
 
 @app.route("/wordlist", methods=["POST", "GET"])
 def serve_wordlist():
-    from_script = request.json["from"]
     to_script = request.json["to"]
-    locale = request.json["locale"]
-    randomized_wordlist = random.choices(wordlists[locale][0], weights=wordlists[locale][1], k=500)
-    form_translit = [[form, transliterate.process(from_script, to_script, form)] for form in randomized_wordlist]
-    return jsonify(form_translit)
+    lang = request.json["lang"]
+    script = request.json["script"]
+    from_script = (
+        script if request.json["from"] == "autodetect" else request.json["from"]
+    )
+    randomized_wordlist = random.choices(
+        wordlists[lang][script][0], weights=wordlists[lang][script][1], k=500
+    )
+    form_translit = [
+        [
+            transliterate.process(script, from_script, form),
+            transliterate.process(script, to_script, form),
+        ]
+        for form in randomized_wordlist
+    ]
+    return jsonify([from_script, to_script, form_translit])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
